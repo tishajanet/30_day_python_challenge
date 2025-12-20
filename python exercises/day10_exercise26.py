63691c655999c6e8a30a798b7e0764a0ec6b0fbd
tishajanet <tishajanet@students.uonbi.ac.ke>
2025-12-20 17:53:45 +0300

    1st commit

--- python exercises/day10_exercise26.py
+++ python exercises/day10_exercise26.py
@@ -5,10 +5,14 @@ import sys
 
 def main():
     values = [5, 6, 7, 8]
+
+    #slice the list to contain the first two items
     first_two = values [0:2]
+
+    #print the new list
     print(first_two)
+
     return 0
 
 if __name__ == "__main__":
-
     sys.exit(main())
