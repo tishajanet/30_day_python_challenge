401c260694f4704e2dcef56c7be253bb88919a35
tishajanet <tishajanet@students.uonbi.ac.ke>
2025-12-20 17:22:04 +0300

    1st commit

---
+++ python exercises/day10_exercise25.py
@@ -0,0 +1,15 @@
+"""Start with "nums = [1, 2, 3]". Append the number 4."""
+import sys
+
+
+def main():
+    nums = [1, 2, 3]
+    nums.append(4) #add the number 4 to the end of the list
+    print(nums)
+
+    return 0
+
+
+if __name__ == "__main__":
+
+    sys.exit(main())
