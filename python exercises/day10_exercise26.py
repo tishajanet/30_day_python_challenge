b67ba933fc4de69c275d384ec67224d182e4e7da
tishajanet <tishajanet@students.uonbi.ac.ke>
2025-12-20 17:39:36 +0300

    1st commit

---
+++ python exercises/day10_exercise26.py
@@ -0,0 +1,14 @@
+"""Given "values = [5, 6, 7, 8]", create "first_two"
+containing only the first two items using slicing."""
+import sys
+
+
+def main():
+    values = [5, 6, 7, 8]
+    first_two = values [0:2]
+    print(first_two)
+    return 0
+
+if __name__ == "__main__":
+
+    sys.exit(main())
