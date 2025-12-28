9423c196733bb7efab8fdbee685ef7aef746b885
tishajanet <tishajanet@students.uonbi.ac.ke>
2025-12-28 04:23:53 +0300

    1st commit

---
+++ python exercises/day15_exercise38.py
@@ -0,0 +1,12 @@
+"""Using the dictionary from the previous exercise,
+ print the result of data.keys()."""
+import sys
+
+
+def main():
+    data = {"a": 1, "b": 2, "c": 3}
+    print(data.keys())
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
