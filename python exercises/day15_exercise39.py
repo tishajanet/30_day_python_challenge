f98f6fa23fc8b5d6a25adf80762332951451e368
tishajanet <tishajanet@students.uonbi.ac.ke>
2025-12-28 04:28:20 +0300

    1st commit

---
+++ python exercises/day15_exercise39.py
@@ -0,0 +1,14 @@
+"""Print the result of data.values().
+"""
+import sys
+
+
+def main():
+    data = {"a": 1, "b": 2, "c": 3}
+
+    #print the result of data.values()
+    print(data.values())
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
