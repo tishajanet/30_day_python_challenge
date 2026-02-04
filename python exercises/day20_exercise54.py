93b70e5f25d8c2e63ac52eb810ddd6f032968c28
tishajanet <tishajanet@students.uonbi.ac.ke>
2026-02-04 18:37:23 +0300

    1st commit

--- python exercises/day20_exercise54.py
+++ python exercises/day20_exercise54.py
@@ -6,6 +6,8 @@ import sys
 
 
 def main():
+
+    #ask the user for input
     first_name = input("Enter your first name: ")
     middle_name = input("Enter your middle name: ")
     last_name = input("Enter your last name: ")
