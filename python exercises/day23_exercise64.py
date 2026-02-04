e682e8b63bac3d04c25ea1341d8823db2b6afcef
tishajanet <tishajanet@students.uonbi.ac.ke>
2026-02-05 01:28:31 +0300

    1st commit

---
+++ python exercises/day23_exercise64.py
@@ -0,0 +1,17 @@
+"""Given a list of integers, print only the positive numbers.
+This will require the use of the 'continue' keyword."""
+import sys
+
+
+def main():
+    integers = [1, -8, 5, 6, -4]
+    for num in integers:
+        #check if the numbers in the list are positive numbers
+        if num <= 0:
+            continue
+        #print the positive numbers
+        print(num)
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
