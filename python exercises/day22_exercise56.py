38321166cbf812873945ccff55f4f90184d64630
tishajanet <tishajanet@students.uonbi.ac.ke>
2026-02-04 22:55:31 +0300

    1st commit

---
+++ python exercises/day22_exercise56.py
@@ -0,0 +1,14 @@
+"""Given:
+numbers = [10, 20, 30, 40]
+Use a for loop to print each number on a new line."""
+import sys
+
+
+def main():
+    numbers = [10, 20, 30, 40]
+    for n in numbers:
+        print(n)
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
