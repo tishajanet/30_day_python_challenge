8335a39a175c695e8e60815a0f7fcc896ba8466a
tishajanet <tishajanet@students.uonbi.ac.ke>
2026-02-24 19:55:14 +0300

    1st commit

--- day28_exercise92.py
+++ day28_exercise92.py
@@ -1,15 +1,14 @@
 """Use the return value of one
  function as the argument to another."""
 import sys
-
+#define a function that returns a name
+def person():
+    return "Tisha"
+#define a function that takes the name and prints it
+def first_name(name):
+    print(name)
 
 def main():
-    #define a function
-    def person():
-        return "Tisha"
-    def first_name(name):
-        print(name)
-
 #use the return value for person() as the argument for first_name
     first_name(person())
     return 0
